11e6e1fb986858812a014939fca810e18f8bb65a
Abdullah al sabbir <91917042+sabbirnrk99@users.noreply.github.com>
2024-11-02 03:14:53 +0600

    speedup index.js

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ app.use(
     credentials: true,
   })
 );
+// Increase request size limit
+app.use(express.json({ limit: "20mb" })); // Adjust the size as needed
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 // Set up multer for file storage
